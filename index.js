d9e5054dc8cd60a40a30a0a3547ba89b57ead816
Carlos Iván Jusdado Tejedor <cijusdado@hps.es>
2020-08-04 10:39:48 +0200

    Se arregla el tema del puerto

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 const path = require('path');
+const port = process.env.PORT || 3000;
 
 // set the path to the public assets
 const publicPath = path.resolve(__dirname, 'public');
@@ -10,6 +11,6 @@ app.get('/pepito', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function () {
+  console.log('Example app listening on port ${port}!');
 });
